9f26e507e6d0607e25a85047338904a6fc33dd9e
jmo-bancaeticalat <154364672+jmo-bancaeticalat@users.noreply.github.com>
2023-12-20 17:09:14 -0300

    Initial Commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,71 @@
+
+<script setup>
+  const text = 'dinámico!';
+  const StyleBackground = 'Background: red';
+  const activo = true;
+
+  const arrayFrutas = ["Manzana","Platano","Sandia","Frutilla","Cerezas"];
+
+  const arrayTiendaFrutas = [
+    {
+    name:'Manzana',
+    price: '$1.00',
+    description: 'Una manzana',
+    id: '1'
+    },
+    {
+    name:'Platano',
+    price: '$2.00',
+    description: 'Una platano',
+    id: '2'
+    },
+    {
+    name:'Sandia',
+    price: '$3.00',
+    description: 'Una sandia',
+    id: '3'
+    },
+
+  ];
+
+
+</script>
+<template>
+  <!-- Las llaves dobles se usan para pasar variables -->
+  <!-- Las llaves dobles permiten expresiones de js .toUperCase en este ejemplo -->
+  <h1>Hola Vue {{ text.toUpperCase() }}</h1>
+  <!-- v-bind o : se usa para insertar atributos en etiquetas -->
+  <h2 :style="StyleBackground">Background red</h2>
+  <h2> 
+    {{ activo ? 'Estoy activo' : 'Estoy inactivo' }}
+  </h2>
+  <h2 v-if="activo === true">Si la variable 'activo' es true, este texto se muestra</h2>
+  <h2 v-else-if="activo === false">Si la variable 'activo' es false llega hasta acá</h2>
+  <h2 v-else>Si la variable es nula o otra opcion, llega hasta aca</h2>
+
+  <hr>
+
+  <!-- En v-for creamos una variable y le indicamos que recorra el arrayFrutas de strings -->
+  <h3>Array listado</h3>
+  <ul>
+    <li v-for="variableFruta in arrayFrutas">
+      {{ variableFruta }}
+    </li>
+  </ul>
+
+  <!-- En el v-for usado en un objeto se usa el atributo 'key' para el elemento unico que no se debe de repetir  -->
+  <!-- Ademas mostramos los elementos de los objetos  -->
+  <h3>Obejeto listado por key</h3>
+  <ul>
+    <li v-for="tiendaFrutas in arrayTiendaFrutas" :key="tiendaFrutas.id">
+      {{ tiendaFrutas.name }} - {{ tiendaFrutas.price }} - {{ tiendaFrutas.description }}
+    </li>
+  </ul>
+
+</template>
+
+<style>
+  h1 {
+    color: aqua;
+  }
+</style>
